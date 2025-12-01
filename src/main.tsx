cef335ec55ed6640aa6895411f01213505cdc42b
sultan-rey <mr.orenald@gmail.com>
2025-12-01 11:21:40 -0500

    second commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { MobileViewport } from "./components/MobileViewport";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <MobileViewport>
-      <App />
-    </MobileViewport>
-  </React.StrictMode>
-);
+  import { createRoot } from "react-dom/client";
+  import App from "./App.tsx";
+  import "./index.css";
+
+  createRoot(document.getElementById("root")!).render(<App />);
